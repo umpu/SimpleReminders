b5db782eb06976762e1a097d6f73e43fee1dc977
umpu <carol.ledgers0h@icloud.com>
2024-02-07 00:07:41 +0500

    Переход на CoreData для хранения данных

---
+++ AddReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  AddReminderView.swift
+//  SimpleReminders
+//
+//  Created by Andrei Gorbunov on 06.02.2024.
+//
+
+import SwiftUI
+
+struct AddReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddReminderView()
+}
---
+++ DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  SimpleReminders
+//
+//  Created by Andrei Gorbunov on 06.02.2024.
+//
+
+import Foundation
